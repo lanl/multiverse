minor bug fixes, covariance plots

examples/helpers.py
@@ -8,6 +8,36 @@ import functorch
 from copy import deepcopy
 import pyro
 
+import seaborn as sns
+
+def covariance_plots(bnn_model):
+    fig, axs = plt.subplots(1,2, figsize=(10,4))
+    axs[0].bar(torch.arange(len(bnn_model.GGN.diag())), (1/bnn_model.GGN.diag()).sqrt(), color='black')
+    # set title
+    axs[0].title.set_text('Approx. posterior std. of $\\theta$')
+
+    for label in axs[0].yaxis.get_ticklabels()[::2]:
+        label.set_visible(False)
+
+    symm_cov = bnn_model.GGN.inverse()
+    std_dev = torch.sqrt(torch.diag(symm_cov))
+    inv_std_dev = 1.0 / std_dev
+    correlation_matrix = symm_cov * (inv_std_dev.unsqueeze(0) * inv_std_dev.unsqueeze(1))
+
+    # plot covariance matrix with seaborn
+    plot2 = sns.heatmap(correlation_matrix, cmap='seismic', vmin=-1, vmax=1)
+    axs[1] = plot2
+    
+    axs[1].set_title('Approx. posterior correlation matrix of $\\theta$')
+
+    # title('Approx. posterior correlation matrix of $\\theta$')
+    # disable ticsk
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.show()
+
+
 def plot_nll(nll_hist, test_nll_hist=None, title = None, save = False, save_path = None):
     ''' Plots the negative log-likelihood history of the model during training.
     '''
@@ -202,7 +232,6 @@ def coverage(predictive, x_test, y_test, x_train, y_train, M=100):
     cp_test = torch.sum(cover_test, dim=0)/N_test
     cp_train = torch.sum(cover_train, dim=0)/N_train
     return cp_train, cp_test
-
 
 
 def loss_on_2d_subspace(data_loader, model, weights, resolution = 50, u_lim = 30, l_lim = -5, MC_chains = None):

inference/bnn.py
@@ -368,7 +368,7 @@ class LaplaceBNN(_SupervisedBNN):
             prior_scale = self.prior._distribution.scale
             P = (1/self.S_perc)*(prior_scale**(-2)) * torch.eye(num_par, device = self.device)
         else:
-            P = - (1/self.S_perc)*hessian(net_tr.log_prob_sum(), self.net_guide.loc, approx=self.approximation).to(self.device)
+            P = - (1/self.S_perc)*hessian(net_tr.log_prob_sum(), self.net_guide.loc).to(self.device)
 
         torch.nn.utils.vector_to_parameters(self.map_params, self.torch_net.parameters())
         self.jacobian = partial(backpack_jacobian, self.torch_net)
@@ -463,7 +463,7 @@ class LaplaceBNN(_SupervisedBNN):
         # if nan try numpy
         if torch.isnan(log_det_H):
             npGGN = self.GGN.cpu().detach().numpy()
-            log_det_H = torch.tensor(np.linalg.slogdet((1/(2*np.pi)) * npGGN).prod()).to(self.device)
+            log_det_H = torch.tensor(np.linalg.slogdet((1/(2*np.pi)) * npGGN)).prod().to(self.device)
         log_ml = log_joint - 0.5 * log_det_H
         return log_ml
 

inference/util.py
@@ -227,6 +227,5 @@ def hessian(y, xs):
             [Hij.reshape(-1) for Hij in torch.autograd.grad(dyi, xs, retain_graph=True)]
         )
         H.append(Hi)
-    H = torch.stack(H)
-
+    H = torch.stack(H)        
     return H